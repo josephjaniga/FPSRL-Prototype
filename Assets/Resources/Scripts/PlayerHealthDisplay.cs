51f4b15e687c31948117b464e95f1e700cc76b28
josephjaniga <josephjaniga@gmail.com>
2015-04-06 20:53:26 -0400

    updated the prototype

--- Assets/Resources/Scripts/PlayerHealthDisplay.cs
+++ Assets/Resources/Scripts/PlayerHealthDisplay.cs
@@ -4,11 +4,24 @@ using System.Collections;
 
 public class PlayerHealthDisplay : MonoBehaviour {
 
+	public Slider slider;
+	public Health unitHealth;
 	public Text textBox;
-	
+
+	void Start()
+	{
+		unitHealth = _.playerHealth;
+	}
+
 	// Update is called once per frame
-	void Update () {
-		textBox.text = "<b>" + _.playerHealth.currentHealth + "<color=red> / </color>" + _.playerHealth.maximumHealth + "<color=red> <3 </color></b>";
+	void Update () 
+	{
+		textBox.text = "<b>" + _.playerHealth.currentHealth + " / " + _.playerHealth.maximumHealth + "<color=red> <3 </color></b>";
+
+		// update health value
+		slider.minValue = 0;
+		slider.maxValue = unitHealth.maximumHealth;
+		slider.value = unitHealth.currentHealth;
 	}
 
 }
