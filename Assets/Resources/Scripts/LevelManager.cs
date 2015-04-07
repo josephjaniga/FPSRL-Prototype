33daf8476ec07a5a8f787f1ab934d854fc337bc9
josephjaniga <josephjaniga@gmail.com>
2015-04-07 00:06:44 -0400

    Rehashed with a new git ignore cleanup to remove the temp files

--- Assets/Resources/Scripts/LevelManager.cs
+++ Assets/Resources/Scripts/LevelManager.cs
@@ -14,6 +14,10 @@ public class LevelManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		if ( Input.GetKeyDown (KeyCode.Escape) ){
+			Application.Quit ();
+		}
+
 	}
 
 	public void init(){
