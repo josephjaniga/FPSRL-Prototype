3db6e80b6c5ebdbc7fc082219c415d0dce3f989f
josephjaniga <josephjaniga@gmail.com>
2015-04-12 23:49:16 -0400

    Updating the equipment for the weapon attributes

--- Assets/Resources/Scripts/Equipment.cs
+++ Assets/Resources/Scripts/Equipment.cs
@@ -5,13 +5,51 @@ public class Equipment : MonoBehaviour {
 
 	public int cash;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Weapons equippedWeapon = Weapons.Pistol;
+
+    // weapon ownership
+    public bool hasPistol = true;
+    public bool hasSword = false;
+
+    // weapon base damage
+    private int _pistolBaseDamage = 1;
+    private float _pistolCritChance = 0f;
+    private float _pistolCritDamage = 0f;
+
+    // pistol enhancements
+    public int pistolDamageEnhancements = 0;
+
+    public int calculatedDamage
+    {
+        get {
+            int calcDmg = 0;
+
+            switch (equippedWeapon)
+            {
+                default:
+                    break;
+                case Weapons.Pistol:
+                    calcDmg = _pistolBaseDamage;  
+                    calcDmg += 
+                    break;
+                case Weapons.Sword:
+                    break;
+            }
+
+            // roll crits
+
+            // apply crit damage
+
+            // apply weapon effects
+
+            return calcDmg;
+        }
+    }
+
+}
+
+public enum Weapons
+{
+    Pistol,
+    Sword
 }
