fb6797cac109a946978243ba6410158da65657c8
josephjaniga <josephjaniga@gmail.com>
2015-04-15 20:09:15 -0400

    updates to the weapon upgrade ui

---
+++ Assets/Resources/Scripts/PistolUpgrade.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PistolUpgrade : MonoBehaviour {
+
+    public int baseCost = 4;
+    public int costScale;
+
+    public Text damageUpgrade;
+    public Text attackSpeedUpgrade;
+    public Text criticalChanceUpgrade;
+    public Text criticalDamageUpgrade;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        damageUpgrade.text = "$" + Mathf.Pow(baseCost, _.playerEquipment.pistolDamageLevel);
+        attackSpeedUpgrade.text = "$" + Mathf.Pow(baseCost, _.playerEquipment.pistolROFLevel);
+        criticalChanceUpgrade.text = "$" + Mathf.Pow(baseCost, _.playerEquipment.pistolCritLevel);
+        criticalDamageUpgrade.text = "$" + Mathf.Pow(baseCost, _.playerEquipment.pistolCritDamageLevel);
+    }
+
+    public void upgradeWeaponDamage()
+    {
+        // get the current level
+        // calculate the cost?
+        // 
+    }
+
+}
